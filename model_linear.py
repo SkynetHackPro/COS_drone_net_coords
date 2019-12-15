67060e7754dee3b0a74140485aed172ef4b2a913
nikolay <yaminsky96@gmail.com>
2019-12-15 21:52:10 +0300

    fix linear model

--- model_linear.py
+++ model_linear.py
@@ -4,30 +4,20 @@ from torch import nn
 class TrackModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.cnn = nn.Sequential(
-            nn.Conv2d(1, 3, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(3),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(3, 9, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(9),
-            nn.Conv2d(9, 21, kernel_size=1, padding=0),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(21),
-            nn.AvgPool2d(kernel_size=2)
-        )
 
-        cnn_out_count = 21
+        input_count = 16
 
         self.net = nn.Sequential(
-            nn.Linear(cnn_out_count, 128),
+            nn.Linear(input_count, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 64),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.4),
-            nn.Linear(128, 2),
+            nn.Linear(64, 2),
         )
 
     def forward(self, x):
-        x = self.cnn(x)
         x = x.view(x.size(0), -1)
         return self.net(x)
